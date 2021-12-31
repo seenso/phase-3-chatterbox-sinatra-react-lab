5b3f1af8e19f4490310857a3768e56107244fba1
seenso <>
2021-12-31 11:34:40 -0900

    migrated messages table

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+
+  #GET
+  get '/messages' do
+    #query
+    #return json response
+  end
+
+  #POST
+  post '/messages' do
+    #create
+    #return json response
+  end
+
+  #PATCH
+  patch '/messages/:id' do
+    #find
+    #update
+    #return json response
+  end
+
+  #DELETE
+  delete '/messages/:id' do
+    #find
+    #delete
+    #return json response
+  end
   
 end
