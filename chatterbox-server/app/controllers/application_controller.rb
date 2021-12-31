97516b1e37acd643b13a4c7989c4177bed3e7244
seenso <>
2021-12-31 11:53:48 -0900

    Completed assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -5,28 +5,42 @@ class ApplicationController < Sinatra::Base
 
   #GET
   get '/messages' do
-    #query
-    #return json response
+    #query & return json response
+    Message.all.to_json
   end
 
   #POST
   post '/messages' do
     #create
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
     #return json response
+    message.to_json
   end
 
   #PATCH
   patch '/messages/:id' do
     #find
+    message = Message.all.find(params[:id])
     #update
+    message.update(
+      body: params[:body],
+      username: params[:username]
+    )
     #return json response
+    message.to_json
   end
 
   #DELETE
   delete '/messages/:id' do
     #find
+    message = Message.all.find(params[:id])
     #delete
+    message.destroy
     #return json response
+    message.to_json
   end
   
 end
